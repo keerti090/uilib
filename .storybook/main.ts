f806f7f6dbe942de509bc292ed38e200e10c267f
keerti Hegde <kirti.vishwanath21@gmail.com>
2026-02-15 13:34:51 -0500

    storybook commits

---
+++ .storybook/main.ts
@@ -0,0 +1,14 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+  ],
+  framework: {
+    name: '@storybook/angular',
+    options: {},
+  },
+};
+
+export default config;
