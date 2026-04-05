c56c8205ec00168280814386205dce0cae8f7cb9
keerti Hegde <kirti.vishwanath21@gmail.com>
2026-04-05 14:25:21 -0400

    some fix related to chromatic

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,9 +2,7 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-essentials',
-  ],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/angular',
     options: {},
